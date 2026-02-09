568c7715e423065d8505fb57d0e1df1c7a91a261
Lyes Mouhoun <lyesmouhoundz@gmail.com>
2026-02-09 23:08:53 +0100

    Mise à jour portfolio: ajout projet Mine Test et Mairie, missions stages, logo Turgot

--- contact.php
+++ contact.php
@@ -1,36 +1,26 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $mail = new PHPMailer(true);
-
-    try {
-        // Paramètres serveur
-        $mail->isSMTP();
-        $mail->Host       = 'ssl0.ovh.net';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'contact@e-portfolyes.fr'; // ton email OVH
-        $mail->Password   = 'Thecontact3105.';       // mot de passe OVH
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
-        $mail->Port       = 587; // ou 465 si SSL
-
-        // Destinataire
-        $mail->setFrom($_POST['email'], $_POST['name']);
-        $mail->addAddress('contact@e-portfolyes.fr'); // où tu reçois les mails
-
-        $mail->Body = "Nom : " . $_POST['name'] . "\n";
-$mail->Body .= "Email : " . $_POST['email'] . "\n";
-$mail->Body .= "Message : " . $_POST['message'];
-
-$mail->send();
-echo "<p style='color:green;'>Merci, votre message a été envoyé avec succès ! Je prends bien note et vous répondrai dès que possible.</p>";
-
-} catch (Exception $e) {
-    echo "<p style='color:red;'>Erreur lors de l'envoi : {$mail->ErrorInfo}</p>";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    $to = 'contact@e-portfolyes.fr';
+    $subject = 'Nouveau message portfolio - ' . $name;
+    
+    $body = "Nom : " . $name . "\n";
+    $body .= "Email : " . $email . "\n\n";
+    $body .= "Message :\n" . $message;
+    
+    $headers = "From: contact@e-portfolyes.fr\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    
+    if (mail($to, $subject, $body, $headers)) {
+        header('Location: index.html?success=1#contact');
+        exit;
+    } else {
+        echo "Erreur lors de l'envoi du message.";
+    }
 }
-}
+?>
